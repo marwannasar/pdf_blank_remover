7cc829174e9db4c35d738c52c69f198a9d3c8be3
SabeehM <sabeeh.hm@gmail.com>
2020-12-31 20:23:48 -0500

    download implemented

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, send_file, send_from_directory, safe_join, abort, redirect, url_for
 from flask_restful import Api, Resource, request
-import os, pathlib, datetime
+import os, pathlib, datetime, time
 
 uploads_path = "uploads"
 allowed_extensions = [".pdf"]
@@ -15,6 +15,9 @@ def correct_name(name):
     return (name[-4:] in allowed_extensions)
 
 
+def temp_func():
+    time.sleep(5)
+
 @app.route("/")
 def home():
     return render_template("/index.html")
@@ -23,14 +26,25 @@ def home():
 def post_upload():
     return render_template("/upload.html")
 
+@app.route("/downloadfile/<filename>", methods=["GET"])
+def download_file(filename):
+    temp_func()
+    return render_template('upload.html',value=filename)
+
+@app.route('/return-files/<filename>')
+def return_files_tut(filename):
+    file_path = uploads_path + "\\" + filename
+    return send_file(file_path, as_attachment=True, attachment_filename='')
 
 @app.route("/api/v1.0/tasks/render/", methods=["POST"])
 def upload():
     file = request.files["file"]
     if (correct_name(file.filename)):
         #file.save(os.path.join(app.config["uploads_path"], file.filename[:-4] + "-input.pdf"))
-        file.save(os.path.join(THIS_FOLDER, uploads_path, file.filename[:-4] + "-" +str(datetime.datetime.now()).replace(":","-").replace(" ", "") + "-input.pdf"))
-    return redirect(url_for('post_upload'))
+        currentDate = str(datetime.datetime.now()).replace(":","-").replace(" ", "")
+        file.save(os.path.join(THIS_FOLDER, uploads_path, file.filename[:-4] + "-" + currentDate+ "-input.pdf"))
+        return redirect("/downloadfile/" + file.filename[:-4] + "-" + currentDate + "-input.pdf")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
